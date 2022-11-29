1f0db2dbaa1b75be7820a1f88f615b4725a27b0e
HoangAnh271 <95622554+HoangAnh271@users.noreply.github.com>
2022-11-29 13:47:42 +0700

    Update commandline.cpp

--- commandline.cpp
+++ commandline.cpp
@@ -62,10 +62,10 @@ void GenerateData(int a[], int n, int dataType)
     case 0: // ngẫu nhiên
         GenerateRandomData(a, n);
         break;
-    case 1: // gần như có thứ tự 
+    case 1: // gần như có thứ tự
         GenerateNearlySortedData(a, n);
         break;
-    case 2: // có thứ tự 
+    case 2: // có thứ tự
         GenerateSortedData(a, n);
         break;
     case 3: // có thứ tự ngược
@@ -175,23 +175,23 @@ void readInputFile(int *&array, int &n, string file_name)
 void print_output_compare_mode(float running_time1, float running_time2, unsigned long long comparison1, unsigned long long comparison2)
 {
     cout << "\n----------------------------\n";
-    cout << "Running time: " << running_time1 << " s | " << running_time2 << " s" << endl;
+    cout << "Running time: " << running_time1 << " milisecond | " << running_time2 << " milisecond" << endl;
     cout << "Comparisions: " << comparison1 << "  |  " << comparison2 << endl;
 }
 
-void print_output_paramenter(int output_paramenter, auto running_time, unsigned long long comparison)
+void print_output_paramenter(int output_paramenter, float running_time, unsigned long long comparison)
 {
     cout << "\n----------------------------\n";
     switch (output_paramenter)
     {
     case 0:
-        cout << "Running time: " << running_time << " s" << endl;
+        cout << "Running time: " << running_time << " milisecond" << endl;
         break;
     case 1:
         cout << "Comparisons: " << comparison << endl;
         break;
     case 2:
-        cout << "Running time: " << running_time << " s" << endl;
+        cout << "Running time: " << running_time << " milisecond" << endl;
         cout << "Comparisons: " << comparison << endl;
         break;
     default:
@@ -263,7 +263,7 @@ void sortWithAlgorithm(int array[], int n, int algorithm)
         mergeSort(array, 0, n - 1);
         break;
     case 6:
-        quickSort(array,0, n - 1);
+        quickSort(array, 0, n - 1);
         break;
     case 7:
         radixSort(array, n);
@@ -325,13 +325,13 @@ void countComparisonAlgorithm(int array[], int n, int algorithm, unsigned long l
         countComparisonRadixSort(array, n, count_comparison);
         break;
     case 8:
-        countComparisonShakerSort(array, 0, n - 1, count_comparison);
+        countComparisonShakerSort(array, n, count_comparison);
         break;
     case 9:
-        countComparisonShellSort(array, 0, n - 1, count_comparison);
+        countComparisonShellSort(array, n, count_comparison);
         break;
     case 10:
-        countComparisonCountingSort(array, 0, n - 1, count_comparison);
+        countComparisonCountSort(array, n, count_comparison);
         break;
     case 11:
         countComparisonFlashSort(array, n, count_comparison);
@@ -353,20 +353,20 @@ void command_line1(int argc, char *argv[])
     cout << "Input size: " << n;
 
     int algorithm_name = algorithmNameToNumber(argv[2]),
-    output_parameter = outParameterToNumber(argv[4]);
+        output_parameter = outParameterToNumber(argv[4]);
 
+    float running_time = 0;
     unsigned long long count_comparison = 0;
 
-
-    auto start_time = high_resolution_clock::now();
+    clock_t start_time, end_time;
+    start_time = clock();
     sortWithAlgorithm(b, n, algorithm_name);
-    auto end_time = high_resolution_clock::now(); 
+    end_time = clock();
     b = copyArray(a, n);
-    auto running_time = duration_cast<milliseconds>(end_time - start_time);
-
+    running_time = (((float)end_time - start_time) / CLOCKS_PER_SEC) * 1000;
     countComparisonAlgorithm(b, n, algorithm_name, count_comparison);
-    writeFile(b, n, "output.txt");
     print_output_paramenter(output_parameter, running_time, count_comparison);
+    writeFile(b, n, "output.txt");
 }
 
 void command_line2(int argc, char *argv[])
@@ -393,8 +393,8 @@ void command_line2(int argc, char *argv[])
     end_time = clock();
     running_time = (((float)end_time - start_time) / CLOCKS_PER_SEC) * 1000;
     countComparisonAlgorithm(a, n, algorithm_name, count_comparison);
-    writeFile(b, n, "output.txt");
     print_output_paramenter(output_parameter, running_time, count_comparison);
+    writeFile(b, n, "output.txt");
 }
 
 void command_line3(int argc, char *argv[])
@@ -501,5 +501,4 @@ void command_line5(int argc, char *argv[])
     b = copyArray(a, n);
     countComparisonAlgorithm(b, n, algorithm2_name, count_comparison2);
     print_output_compare_mode(running_time1, running_time2, count_comparison1, count_comparison2);
-    
 }
